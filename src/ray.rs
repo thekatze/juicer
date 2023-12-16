05efd740e5e62255dd6f7512f1055f690803e059
Leo Katzengruber <leofel.katz@gmail.com>
2023-12-16 15:20:39 +0100

    test for sphere intersection

--- src/ray.rs
+++ src/ray.rs
@@ -16,9 +16,26 @@ impl Ray {
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
     */
     pub fn color(&self) -> Vector<3, f32> {
+        if self.hits_sphere(Vector([0.0, 0.0, -1.0]), 0.5) {
+            return Vector([1.0, 0.0, 0.0]);
+        }
+
         let unit_direction = self.direction.normalize();
         let a = 0.5 * (unit_direction.y() + 1.0);
-        
+
         Vector([1.0, 1.0, 1.0]) * (1.0 - a) + Vector([0.5, 0.7, 1.0]) * a
+    }
+
+    fn hits_sphere(&self, center: Vector<3, f32>, radius: f32) -> bool {
+        let sphere_direction = self.start.clone() - center;
+
+        // solve using quadratic formula
+        let a = self.direction.dot(&self.direction);
+        let b = 2.0 * sphere_direction.dot(&self.direction);
+        let c = sphere_direction.dot(&sphere_direction) - radius * radius;
+
+        let discriminant = b * b - 4.0 * a * c;
+
+        discriminant > 0.0
     }
 }
